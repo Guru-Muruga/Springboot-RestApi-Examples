b526fe3003852016addb74d28e6b1c0d746e362e
Guru-Muruga <123896468+Guru-Muruga@users.noreply.github.com>
2026-01-06 02:50:10 +0530

    implemented SLF4J logging in my Spring Boot REST API

--- src/main/java/com/example/sb/first/controller/FirstRestController.java
+++ src/main/java/com/example/sb/first/controller/FirstRestController.java
@@ -1,5 +1,7 @@
 package com.example.sb.first.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstRestController {
 
+    private static final Logger logger= LoggerFactory.getLogger(FirstRestController.class);
     @GetMapping("/hello")
     public String hello(){
+        logger.info("Received request for /hello");
         return "Hello, Welcome to the Spring Boot!";
     }
 
     @GetMapping ("/hello/{name}")
-        public String helloWithName(@PathVariable String name){
-            return "Hello! " + name + "!, welcome to Spring Boot's world!";
+    public String helloWithName(@PathVariable String name){
+        logger.info("Received request for /hello/{}", name);
+        return "Hello, " + name + "!, welcome to Spring Boot's world!";
     }
 }
